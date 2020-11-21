cffca0dc472e0fd27be00c765c864796631d6b36
Brice Videau <bvideau@anl.gov>
2020-11-21 11:27:51 -0600

    Added test for stream.

---
+++ test/babeltrace2/trace-ir/stream.rb
@@ -0,0 +1,94 @@
+class BTStreamTest < Minitest::Test
+
+  def test_trace_class
+    stream_beginning_count = 0
+    stream_end_count = 0
+    consume = lambda { |iterator, _|
+      mess = iterator.next_messages
+      mess.each { |m|
+        case m.type
+        when :BT_MESSAGE_TYPE_STREAM_BEGINNING
+          stream_beginning_count += 1
+        when :BT_MESSAGE_TYPE_STREAM_END
+          stream_end_count += 1
+        end
+      }
+    }
+
+    states = [
+      :BT_MESSAGE_TYPE_STREAM_BEGINNING,
+      :BT_MESSAGE_TYPE_STREAM_END ]
+    index = 0
+    trace_class = nil
+    stream_class = nil
+    trace = nil
+    stream = nil
+
+    next_method = lambda { |it, capacity|
+      return [] if capacity == 0
+      m = case states[index]
+        when :BT_MESSAGE_TYPE_STREAM_BEGINNING
+          BT2::BTMessage::StreamBeginning.new(self_message_iterator: it, stream: stream)
+        when :BT_MESSAGE_TYPE_STREAM_END
+          BT2::BTMessage::StreamEnd.new(self_message_iterator: it, stream: stream)
+        when nil
+          raise StopIteration
+        else
+          raise "invalid state"
+        end
+      index += 1
+      [m]
+    }
+
+    comp_initialize_method = lambda { |self_component, configuration, params, data|
+      self_component.add_output_port("p0")
+      trace_class = BT2::BTTraceClass.new(self_component: self_component)
+      stream_class = BT2::BTStreamClass.new(trace_class: trace_class)
+      stream_class.assigns_automatic_stream_id = false
+      trace = BT2::BTTrace.new(trace_class: trace_class)
+      stream = BT2::BTStream.new(stream_class: stream_class, trace: trace, id: 25)
+      assert_instance_of(BT2::BTStream, stream)
+      assert_equal(stream_class, stream.get_class)
+      assert_equal(trace, stream.trace)
+      assert_equal(25, stream.id)
+      assert_nil(stream.name)
+      stream.name = "name"
+      assert_equal("name", stream.name)
+      assert_equal({}, stream.user_attributes.value)
+      stream.user_attributes = { "foo" => 15 }
+      assert_equal({ "foo" => 15 }, stream.user_attributes.value)
+    }
+
+    fini_done = false
+    comp_finalize_method = lambda { |self_component|
+      fini_done = true
+    }
+
+    iter_class = BT2::BTMessageIteratorClass.new(next_method: next_method)
+    source_class = BT2::BTComponentClass::Source.new(name: "empty_stream", message_iterator_class: iter_class)
+    source_class.initialize_method = comp_initialize_method
+    source_class.finalize_method = comp_finalize_method
+
+    graph = BT2::BTGraph.new
+    comp1 = graph.add(source_class, "source")
+    comp2 = graph.add_simple_sink("count", consume)
+    op = comp1.output_port(0)
+    ip = comp2.input_port(0)
+    graph.connect_ports(op, ip)
+    graph.run
+    assert_equal(1, stream_beginning_count)
+    assert_equal(1, stream_end_count)
+    source_class = nil
+    iter_class = nil
+    graph = nil
+    comp1 = nil
+    comp2 = nil
+    op = ip = nil
+    trace_class = nil
+    stream_class = nil
+    trace = nil
+    stream = nil
+    GC.start
+    assert(fini_done)
+  end
+end
